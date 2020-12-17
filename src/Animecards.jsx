52c3a08c263fab949e63e468c41a197a9d229144
Prakhar Agrawal <prakharagrawal023@gmail.com>
2020-12-17 17:58:31 +0530

    button css added little responsiveness needed

--- src/Animecards.jsx
+++ src/Animecards.jsx
@@ -9,9 +9,10 @@ function Card(props){
                 <img src = {props.imgsrc} alt="Naruto" className="card_img" />
                 <div className="cardinfo">
                     <h3 className="serialname">{props.sname}</h3>
-                    <span className="genre">{props.genre}</span>
+                    <div className="genre"><span>{props.genre}</span></div>
+                    <br/>
                     <a href={props.link} target ="blank">
-                        <button>Watch Now</button>
+                        <button className="btn-hover">Watch Now</button>
                     </a>
                 </div>
             </div>
